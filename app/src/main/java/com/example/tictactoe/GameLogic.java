c31e3f53a2375133ccec4f743b213ce8ae44dae6
paula-sfr <79749186+paula-sfr@users.noreply.github.com>
2021-05-14 13:12:04 +0200

    ttt commit 3

--- app/src/main/java/com/example/tictactoe/GameLogic.java
+++ app/src/main/java/com/example/tictactoe/GameLogic.java
@@ -21,6 +21,7 @@ public class GameLogic {
     private int player = 1;
 
     GameLogic(){
+
         gameBoard = new int[3][3];
         for (int r = 0; r < 3; r++){
             for (int c = 0; c < 3; c++){
